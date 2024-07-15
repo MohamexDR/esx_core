9eaf88fd12defe934530f1e0850b56cd78301463
MohamexDR <96547404+MohamexDR@users.noreply.github.com>
2024-07-16 00:56:00 +0300

    Update common.lua
    allowing the player to use the shared object without the need of an export using the old ways

--- [core]/es_extended/server/common.lua
+++ [core]/es_extended/server/common.lua
@@ -18,10 +18,14 @@ RegisterNetEvent("esx:onPlayerSpawn", function()
     ESX.Players[source].spawned = true
 end)
 
-AddEventHandler("esx:getSharedObject", function()
+--[[AddEventHandler("esx:getSharedObject", function()
     local Invoke = GetInvokingResource()
     print(("[^1ERROR^7] Resource ^5%s^7 Used the ^5getSharedObject^7 Event, this event ^1no longer exists!^7 Visit https://documentation.esx-framework.org/tutorials/tutorials-esx/sharedevent for how to fix!"):format(Invoke))
-end)
+end)]]
+
+AddEventHandler("esx:getSharedObject", function(cb)
+    cb(ESX) 
+end) -- allowing the player to get the shared object without the new export :) 
 
 exports("getSharedObject", function()
     return ESX
