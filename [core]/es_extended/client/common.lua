c87e4c8437977e318f0f6a713fb3323e1deaf78d
MohamexDR <96547404+MohamexDR@users.noreply.github.com>
2024-07-16 00:56:47 +0300

    Update common.lua
    same as common.lua in server side

--- [core]/es_extended/client/common.lua
+++ [core]/es_extended/client/common.lua
@@ -2,7 +2,10 @@ exports("getSharedObject", function()
     return ESX
 end)
 
-AddEventHandler("esx:getSharedObject", function()
+--[[AddEventHandler("esx:getSharedObject", function()
     local Invoke = GetInvokingResource()
     print(("[^1ERROR^7] Resource ^5%s^7 Used the ^5getSharedObject^7 Event, this event ^1no longer exists!^7 Visit https://documentation.esx-framework.org/tutorials/tutorials-esx/sharedevent for how to fix!"):format(Invoke))
-end)
+end)]]
+AddEventHandler("esx:getSharedObject", function(cb)
+    cb(ESX) 
+end) -- same as common.lua in server side
